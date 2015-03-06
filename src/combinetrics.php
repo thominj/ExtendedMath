a142e8d817b033e5b0dd280be81e98aa29adf1fd
Michael Cordingley (mcordingley) <Michael.Cordingley@gmail.com>
2015-03-05 22:31:01 -0500

    Port some functions over from PHPStats, but rename them.

--- src/combinetrics.php
+++ src/combinetrics.php
@@ -1,2 +1,62 @@
 <?php
 
+if (!function_exists('fact')) {
+    /**
+     * fact
+     * 
+     * Returns the factorial of an integer.
+     * 
+     * @param int $x 
+     * @return int The factorial of $x, i.e. x!
+     */
+    function fact($x) {
+        if ($x < 0) {
+            throw new InvalidArgumentException("fact() only takes non-negative integers, $x provided.");
+        }
+        
+        $x = floor($x);
+        
+        $sum = 1;
+        
+        for ($i = 1; $i <= $x; $i++) {
+            $sum *= $i;
+        }
+        
+        return $sum;
+    }
+}
+
+if (!function_exists('npr')) {
+    /**
+     * npr
+     * 
+     * Permutation function. Returns the number of ways of choosing $r objects
+     * from a collection of $n objects, where the order of selection matters.
+     * 
+     * @param int $n The size of the collection
+     * @param int $r The size of the selection
+     * @return int $n pick $r
+     * @static
+     */
+    function npr($n, $r) {
+        return fact($n) / fact($n - $r);
+    }
+}
+
+if (!function_exists('ncr')) {
+    /**
+     * ncr
+     * 
+     * Combination function Returns the number of ways of choosing $r objects
+     * from a collection of $n objects, where the order of selection does not
+     * matter.
+     * 
+     * @param int $n The size of the collection
+     * @param int $r The size of the selection
+     * @return int $n choose $r
+     * @static
+     */
+    function ncr($n, $r) {
+        return npr($n, $r) / fact($r);
+    }
+}
--- tests/CombinetricsTest.php
+++ tests/CombinetricsTest.php
@@ -1,2 +1,31 @@
 <?php
 
+class CombinetricsTest extends PHPUnit_Framework_TestCase
+{
+    public function test_fact()
+    {
+        $this->assertEquals(1, fact(0));
+        $this->assertEquals(1, fact(1));
+        $this->assertEquals(2, fact(2));
+        $this->assertEquals(120, fact(5));
+        $this->assertEquals(3628800, fact(10));
+    }
+
+    public function test_npr()
+    {
+        $this->assertEquals(1, npr(1, 1));
+        $this->assertEquals(2, npr(2, 1));
+        $this->assertEquals(12, npr(4, 2));
+        $this->assertEquals(120, npr(5, 5));
+        $this->assertEquals(6720, npr(8, 5));
+    }
+    
+    public function test_ncr()
+    {
+        $this->assertEquals(1, ncr(1, 1));
+        $this->assertEquals(2, ncr(2, 1));
+        $this->assertEquals(6, ncr(4, 2));
+        $this->assertEquals(1, ncr(5, 5));
+        $this->assertEquals(56, ncr(8, 5));
+    }
+}
