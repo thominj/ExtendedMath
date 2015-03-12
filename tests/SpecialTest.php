d08032069aa7e49ec43124a6fc21d42693dcffc3
Michael Cordingley <Michael.Cordingley@gmail.com>
2015-03-11 21:23:42 -0400

    Add inverse error function.

--- tests/SpecialTest.php
+++ tests/SpecialTest.php
@@ -1,2 +1,25 @@
 <?php
 
+class SpecialTest extends PHPUnit_Framework_TestCase
+{
+    
+
+    /* Depends on erf being present
+    public function test_ierf()
+    {
+        $this->assertTrue(Double.isNaN(ierf(-1.001)));
+        $this->assertTrue(Double.isNaN(ierf(1.001)));
+
+        $this->assertTrue(Double.isInfinite(ierf(-1)));
+        $this->assertTrue(ierf(-1) < 0);
+        $this->assertTrue(Double.isInfinite(ierf(1)));
+        $this->assertTrue(ierf(1) > 0);
+
+        for ($x = -5.9; $x < 5.9; $x += 0.01) {
+            $y = erf($x);
+            $dydx = 2 * exp(-$x * $x) / sqrt(M_PI);
+            $this->assertEquals($x, ierf($y), '', 1.0e-15 / $dydx);
+        }
+    }
+    */
+}
