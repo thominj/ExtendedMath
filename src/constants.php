2192f28ddfdb742195bfb38ca3758aaac9213b0d
Michael Cordingley <Michael.Cordingley@gmail.com>
2015-03-11 23:46:00 -0400

    Add some useful constants.

---
+++ src/constants.php
@@ -0,0 +1,16 @@
+<?php
+
+if (!defined('M_LOG2PI_2')) {
+    // log(2 * PI) / 2
+    define('M_LOG2PI_2', 0.91893853320467);
+}
+
+if (!defined('M_SQRT2PI')) {
+    // sqrt(2 * M_PI)
+    define('M_SQRT2PI', 2.506628274631000502);
+}
+
+if (!defined('M_GAMMA')) {
+    // http://en.wikipedia.org/wiki/Euler%E2%80%93Mascheroni_constant
+    define('M_GAMMA', 0.577215664901532860606512090082);
+}
